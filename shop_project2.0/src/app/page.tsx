16c4daff67df4edff10e2909c6ffcc4e48616be1
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2024-02-14 15:01:27 +0400

    completed

--- shop_project2.0/src/app/page.tsx
+++ shop_project2.0/src/app/page.tsx
@@ -2,16 +2,11 @@ import Card from "./components/cardContent/frontPageContent";
 import Header from "./components/main";
 import Review from "./components/review";
 
-type homeProps = {
-  product: any;
-};
-
-export default function Home({ product }: homeProps) {
-  console.log(product);
+export default function Home() {
   return (
     <>
       <Header />
-      <Card product={[product]} />
+      <Card />
       <Review />
     </>
   );
